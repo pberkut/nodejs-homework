6ae0468a6a2595fbfe4444c60b2dbed76bc2f8d0
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-05-25 20:21:03 +0300

    Code refactoring

--- controllers/contacts.controllers.js
+++ controllers/contacts.controllers.js
@@ -8,40 +8,40 @@ const {
 
 const { controllerWrapper } = require('../utils');
 
-const getContacts = async (req, res) => {
+const getContacts = controllerWrapper(async (req, res) => {
   const { _id: owner } = req.user;
   const contacts = await getContactsService(owner, req.query);
   return res.json(contacts);
-};
+});
 
-const getContact = async (req, res) => {
+const getContact = controllerWrapper(async (req, res) => {
   const { contactId } = req.params;
   const contact = await getContactService(contactId);
   return res.json(contact);
-};
+});
 
-const createContact = async (req, res) => {
+const createContact = controllerWrapper(async (req, res) => {
   const { _id: owner } = req.user;
   const newContact = await createContactService({ ...req.body, owner });
   return res.status(201).json(newContact);
-};
+});
 
-const updateContact = async (req, res) => {
+const updateContact = controllerWrapper(async (req, res) => {
   const { contactId } = req.params;
   const updateContact = await updateContactService(contactId, req.body);
   res.json(updateContact);
-};
+});
 
-const deleteContact = async (req, res) => {
+const deleteContact = controllerWrapper(async (req, res) => {
   const { contactId } = req.params;
   await deleteContactService(contactId);
   return res.json({ message: 'Contact deleted' });
-};
+});
 
 module.exports = {
-  getContacts: controllerWrapper(getContacts),
-  getContact: controllerWrapper(getContact),
-  createContact: controllerWrapper(createContact),
-  updateContact: controllerWrapper(updateContact),
-  deleteContact: controllerWrapper(deleteContact),
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
 };
