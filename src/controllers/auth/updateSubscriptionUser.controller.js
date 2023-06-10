8a949e312c7a39cdd71d86b4dc2aaa54061c9d94
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-06-10 22:29:23 +0300

    Bugfix: uploadAvatar, UpdateSubcription

--- src/controllers/auth/updateSubscriptionUser.controller.js
+++ src/controllers/auth/updateSubscriptionUser.controller.js
@@ -1,9 +1,9 @@
 const { controllerWrapper } = require('../../utils');
-const { authService } = require('../../services');
+const { userService } = require('../../services');
 
 const updateSubscriptionUser = controllerWrapper(async (req, res) => {
   const { _id } = req.user;
-  const updateUser = await authService.updateSubscription(_id, req.body);
+  const updateUser = await userService.updateSubscription(_id, req.body);
   res.status(200).json(updateUser);
 });
 
--- src/controllers/auth/uploadAvatarUser.controller.js
+++ src/controllers/auth/uploadAvatarUser.controller.js
@@ -1,19 +1,20 @@
 const fs = require('fs/promises');
 
 const { controllerWrapper } = require('../../utils');
-const { authService } = require('../../services');
+const { userService } = require('../../services');
 const cloudServices = require('../../services/cloud.service');
 
 const uploadAvatarUser = controllerWrapper(async (req, res) => {
   const { _id } = req.user;
   const pathFile = req.file.path;
 
-  const { secure_url: avatarURL, public_id: idCloudAvatar } =
-    await cloudServices.uploadAvatar(pathFile);
+  const { secure_url: avatarURL, public_id: idCloudAvatar } = await cloudServices.uploadAvatar(
+    pathFile,
+  );
 
-  const oldAvatar = await authService.getAvatar(_id);
+  const oldAvatar = await userService.getAvatar(_id);
   await cloudServices.deleteAvatar(oldAvatar);
-  await authService.updateAvatar(_id, avatarURL, idCloudAvatar);
+  await userService.updateAvatar(_id, avatarURL, idCloudAvatar);
   await fs.unlink(pathFile);
 
   res.json({ avatarURL });
